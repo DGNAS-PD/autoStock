f0bf85bed3ae81f1b8e4789b8e21ca6875ee2b84
Changlong He <13518181678@163.com>
2023-09-22 21:05:16 +0800

    前端

---
+++ frontEnd/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <AppNav/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import AppNav from "@/components/AppNav";
+
+export default {
+  name: 'App',
+  components: {AppNav},
+}
+</script>
